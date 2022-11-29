98e542ebca9b3ecda7adc547e61f0f8e009703e4
GrahamTCreighton <guitarrista.1988@hotmail.com>
2022-11-29 11:19:25 +0100

    BrandColors and SemanticColorLibrary

---
+++ src/stories/designComponents/semanticColors.js
@@ -0,0 +1,26 @@
+//all alpha colors need revision to check if they work
+
+const SemanticPalette = {
+  danger: "#e50a1f",
+  dangerShade10: "#ce091c",
+  dangerTint80: "#FACED2",
+  dangerAlpha20: "rgba(229, 10, 31, 0.2)",
+  dangerAlpha5: "rgba(229, 10, 31, 0.05)",
+
+  warning: "#FFC107",
+  warningShade10: "#E6AE06",
+  warningTint80: "#FFF3CD",
+  warningAlpha20: "rgba(255, 193, 7, 0.2)",
+
+  info: "#007BFF",
+  infoShade10: "#006FE6",
+  infoTint80: "#CCE5FF",
+  infoAlpha20: "rgba(0, 123, 255, 0.2)",
+
+  Success: "#00D686",
+  SuccessShade10: "#00C179",
+  SuccessTint80: "#CCF7E7",
+  SuccessAlpha20: "rgba(0, 214, 134, 0.2)",
+};
+
+export default SemanticPalette;
