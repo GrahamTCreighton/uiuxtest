777725a7f1471b5c61b02e219f4a28bf88c52dd8
GrahamTCreighton <guitarrista.1988@hotmail.com>
2022-12-01 10:34:05 +0100

    Storybook theme

---
+++ .storybook/YourTheme.js
@@ -0,0 +1,11 @@
+// .storybook/YourTheme.js
+
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+  brandTitle: "Sendible UI/UX Test",
+  brandImage:
+    "https://f.hubspotusercontent30.net/hubfs/2235233/raw_assets/public/sendible-theme-2020/images/favicon/favicon@512w.png",
+  brandTarget: "_self",
+});
